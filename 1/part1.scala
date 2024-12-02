4f97e9bad9ac51f4096c6825ddfb1a8e68affe98
Axel Samuelsson <asamue12@volvocars.com>
2024-12-02 20:14:25 +0100

    man i missed scala

---
+++ 1/part1.scala
@@ -0,0 +1,11 @@
+object Part1 {
+  def main(args: Array[String]): Unit = {
+    val bufferedSource = io.Source.fromFile("data.txt")
+    val lines = bufferedSource.getLines.map(line => line.split("\\s+").toList).toList
+    val left = lines.map(part => part(0)).map(_.toInt).sorted.toList
+    val right = lines.map(part => part(1)).map(_.toInt).sorted.toList
+    // println(left.mkString("[", ", ", "]"))
+
+    println(left.zip(right).map((a,b) => (a-b).abs).sum)
+  }
+}
